d4f3beef1ed6ce7c2a37ce933557702735ac45a0
Hans-J. Schmid <knock@myopendoor.de>
2025-01-08 19:04:35 +0100

    remove questionaire

--- examples/ch1-03-spawn-closure.rs
+++ examples/ch1-03-spawn-closure.rs
@@ -3,8 +3,8 @@ use std::thread;
 fn main() {
   let numbers = vec![1, 2, 3];
 
-  thread::spawn(move || {
-    for n in &numbers {
+  thread::spawn(|| {
+    for n in numbers {
       println!("{n}");
     }
   })
--- examples/ch1-04-scoped-threads.rs
+++ examples/ch1-04-scoped-threads.rs
@@ -13,4 +13,14 @@ fn main() {
       }
     });
   });
+
+  // let mut numbers = vec![1, 2, 3];
+  // thread::scope(|s| {
+  //   s.spawn(|| {
+  //     numbers.push(1);
+  //   });
+  //   s.spawn(|| {
+  //     numbers.push(2); // Error!
+  //   });
+  // });
 }
