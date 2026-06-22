c6b09e869130eb0e19b6f520a03ab89be76cfa9c
Hans-J. Schmid <knock@myopendoor.de>
2026-06-23 00:58:51 +0200

    reformat

--- ch08/ch8-01-futex.rs
+++ ch08/ch8-01-futex.rs
@@ -6,44 +6,44 @@ use std::thread;
 use std::time::Duration;
 
 pub fn wait(a: &AtomicU32, expected: u32) {
-    // Refer to the futex (2) man page for the syscall signature.
-    unsafe {
-        libc::syscall(
-            libc::SYS_futex, // The futex syscall.
-            a as *const AtomicU32, // The atomic to operate on.
-            libc::FUTEX_WAIT, // The futex operation.
-            expected, // The expected value.
-            std::ptr::null::<libc::timespec>(), // No timeout.
-        );
-    }
+  // Refer to the futex (2) man page for the syscall signature.
+  unsafe {
+    libc::syscall(
+      libc::SYS_futex,                    // The futex syscall.
+      a as *const AtomicU32,              // The atomic to operate on.
+      libc::FUTEX_WAIT,                   // The futex operation.
+      expected,                           // The expected value.
+      std::ptr::null::<libc::timespec>(), // No timeout.
+    );
+  }
 }
 
 pub fn wake_one(a: &AtomicU32) {
-    // Refer to the futex (2) man page for the syscall signature.
-    unsafe {
-        libc::syscall(
-            libc::SYS_futex, // The futex syscall.
-            a as *const AtomicU32, // The atomic to operate on.
-            libc::FUTEX_WAKE, // The futex operation.
-            1, // The number of threads to wake up.
-        );
-    }
+  // Refer to the futex (2) man page for the syscall signature.
+  unsafe {
+    libc::syscall(
+      libc::SYS_futex,       // The futex syscall.
+      a as *const AtomicU32, // The atomic to operate on.
+      libc::FUTEX_WAKE,      // The futex operation.
+      1,                     // The number of threads to wake up.
+    );
+  }
 }
 
 fn main() {
-    let a = AtomicU32::new(0);
+  let a = AtomicU32::new(0);
 
-    thread::scope(|s| {
-        s.spawn(|| {
-            thread::sleep(Duration::from_secs(3));
-            a.store(1, Relaxed);
-            wake_one(&a);
-        });
-
-        println!("Waiting...");
-        while a.load(Relaxed) == 0 {
-            wait(&a, 0);
-        }
-        println!("Done!");
+  thread::scope(|s| {
+    s.spawn(|| {
+      thread::sleep(Duration::from_secs(3));
+      a.store(1, Relaxed);
+      wake_one(&a);
     });
+
+    println!("Waiting...");
+    while a.load(Relaxed) == 0 {
+      wait(&a, 0);
+    }
+    println!("Done!");
+  });
 }
